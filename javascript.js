5dc5b5267c75b3d9085e66329b0883df5722d223
sarthakastic <iamsg26@gmail.com>
2022-10-03 19:45:46 +0530

    home page made responsive and added a burger menu

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 document.getElementById("search").addEventListener("keypress", (e) => {
   if (e.key == "Enter") window.find(e.target.value);
 });
+
+function handleMenu() {
+  
+  let nav = document.getElementById("mobile-nav-display");
+
+  nav.classList.toggle("mobile-nav-display");
+}
